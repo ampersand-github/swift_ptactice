0e63811d10c942e8c15a5b2f95acba53bf7e6b59
ampersand <ampersand.dev@gmail.com>
2020-08-19 03:57:47 +0900

    auto commit

--- SwiftUIPrograming/SwiftUIPrograming/TODO/View/InputSheetView.swift
+++ SwiftUIPrograming/SwiftUIPrograming/TODO/View/InputSheetView.swift
@@ -10,38 +10,58 @@ import SwiftUI
 
 struct InputSheetView: View {
     @ObservedObject var todoVM: TodoViewModel
-    @State var dateFlag: Bool = true
-    @State var timeFlag: Bool = true
-    @State private var selectionDate = Date()
+    @State var dateFlag: Bool = false
+    @State var timeFlag: Bool = false
+    @State private var date = Date()
+    @State private var time = Date()
 
+    
+    // [https://www.appcoda.com/swiftui-form-ui/ SwiftUI Tutorial: How to Build a Form UI for iOS Apps]
+    // でつくりなおしてみる。
+    // formはボタンが置けるけど、デザインがだめ
+    
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("メモのタイトルを入力してください。")) {
-                    InputView(todoVM: self.todoVM)
-                }
-                Section(header: Text("日付の期限を選択してください。")) {
-                    Toggle(isOn: $dateFlag) {
-                        // Image(systemName: "tram.fill")
-                        Text("日付の入力")
+            VStack {
+                Form {
+                    Section(header: Text("メモのタイトルを入力してください。")) {
+                        InputView(todoVM: self.todoVM)
                     }
-                    Group {
-                        if self.dateFlag {
-                            DatePicker("日付を選択", selection: $selectionDate, displayedComponents: .date)
-                        } else {
-                            Text("選択できません").font(.caption)
+                    Section(header: Text("日付の期限を選択してください。")) {
+                        Toggle(isOn: $dateFlag) {
+                            // Image(systemName: "tram.fill")
+                            Text("日付の入力")
+                        }
+                        Group {
+                            if self.dateFlag {
+                                DatePicker("日付を選択", selection: $date, displayedComponents: .date)
+                            } else {
+                                Text("選択できません").font(.caption)
+                            }
                         }
                     }
-                }
-                Section(header: Text("時間の期限を選択してください。")) {
-                    Toggle(isOn: $timeFlag) {
-                        // Image(systemName: "tram.fill")
-                        Text("時間の入力")
+                    Section(header: Text("時間の期限を選択してください。")) {
+                        Toggle(isOn: $timeFlag) {
+                            // Image(systemName: "tram.fill")
+                            Text("時間の入力")
+                        }
+                        Group {
+                            if self.dateFlag {
+                                DatePicker("時刻を選択", selection: $time, displayedComponents: .hourAndMinute)
+                            } else {
+                                Text("選択できません").font(.caption)
+                            }
+                        }
                     }
-                    DatePicker("時刻を選択", selection: $selectionDate, displayedComponents: .hourAndMinute)
+                    Button(action: {
+                        print("Button Tapped")
+                    }) {
+                        Text("Button")
+                    }
                 }
+
+                .navigationBarTitle("フォームタイトル")
             }
-            .navigationBarTitle("フォームタイトル")
         }
     }
 }
